80299bd6d00ef1cfb4c9e4ab4f2da5b425ca33b6
Yugpatel158 <yugpatel1585@gmail.com>
2026-09-24 17:24:54 +0530

    LoopExample File

---
+++ LoopEx.js
@@ -0,0 +1,16 @@
+for (let i = 0; i <=11 ; i++) {
+    console.log(i);    
+}
+
+let i=0;
+while (i<=10) {
+    console.log(i);
+    i++;
+}
+
+let j=0;
+do {
+    console.log(j)
+    j++;
+} while (j<10);
+
